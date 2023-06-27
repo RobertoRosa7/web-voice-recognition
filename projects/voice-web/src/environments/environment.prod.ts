c4cc0e4f355e273612fe7451b10900368b5e384e
Roberto Rosa <roberto.rosa7@gmail.com>
2023-06-27 18:11:28 -0300

    update

--- projects/voice-web/src/environments/environment.prod.ts
+++ projects/voice-web/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: 'https://192.168.0.200:8080',
-  mlApi: 'https://192.168.0.200:5000',
+  api: 'http://192.168.0.200:8080',
+  mlApi: 'http://192.168.0.200:5000',
 };
